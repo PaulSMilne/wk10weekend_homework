2c6be7c338fc1bb6956753e9d597e01b616a6c7d
Paul Milne <hallhill@gmail.com>
2016-12-02 14:45:19 +0000

    downloaded underscore library to get _.countBy for frequency of items in an array

--- record_store.js
+++ record_store.js
@@ -1,14 +1,24 @@
+var _ = require('underscore');
+
 var RecordStore = function(name, city){
      this.name = name;
      this.city = city;
-     this.bankBalance = 0;
+     this.bankBalance = 10000;
      this.inventory = [];
 }
 
 RecordStore.prototype = {
      addRecord: function(record){
           this.inventory.push(record);
-     }
+          this.bankBalance -= record.price/2;
+     },
+
+     getInventory: function(){
+          var stock = _.countBy(this.inventory, function(record) {
+           return record.name;
+          })        
+     return stock;
+     } 
 }
 
 module.exports = RecordStore;
--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -1,6 +1,7 @@
 var RecordStore = require('../record_store');
 var Record = require('../record');
 var assert = require('assert');
+var _ = require('underscore');
 
 var recordStore;
 var record1;
@@ -8,7 +9,8 @@ var record1;
 describe('Record Store', function(){
      beforeEach(function(){
           recordStore = new RecordStore("The Vinyl Frontier", "Dunbar");
-          AreYouExperienced = new Record("Jimi Hendrix", "Are You Experienced", 25);
+          AreYouExperienced = new Record("Jimi Hendrix", "Are You Experienced", 20);
+          BloodOnTheTracks = new Record("Bob Dylan", "Blood on the Tracks", 10);
      });
      it('has a name', function(){
           assert.equal("The Vinyl Frontier", recordStore.name);
@@ -21,10 +23,20 @@ describe('Record Store', function(){
           assert.equal(1, recordStore.inventory.length);
      });
      it('has a bank balance', function(){
-          assert.equal(0, recordStore.bankBalance);
+          assert.equal(10000, recordStore.bankBalance);
      });
+
+     it('decreases bank balance by the wholesale price of the record: 1/2 the retail price.', function(){
+          recordStore.addRecord(AreYouExperienced);
+          assert.equal(9990, recordStore.bankBalance);
+     })
      it('can display inventory', function(){
+          recordStore.addRecord(AreYouExperienced);
+          recordStore.addRecord(AreYouExperienced);
+          recordStore.addRecord(BloodOnTheTracks);
           var inventory = recordStore.getInventory();
-          assert.equal("Jimi Hendrix, Are You Experienced, 25", inventory);
+          assert.deepEqual({ 'Are You Experienced': 2, 'Blood on the Tracks': 1 }, inventory);
      });
 })
+
+     
